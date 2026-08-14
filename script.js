1ad60c471b76a38f243ffa61ccf1b1364d2ddcf8
MariamIdrees <thumininu94@gmail.com>
2026-08-14 22:23:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// write a prompt that tells a user they are eligible to vote
+ let userAge = (prompt("Enter your age"));
+ let newVote = userAge >= 18 ? "you can vote" : "you cannoot vote";
+
+ alert(newVote);
+ console.log(newVote)
+ console.log (parseInt("userAge"))
+
+
+
+let a = parseInt(prompt("Enter your first number"));
+let b = parseInt(prompt("Enter your second number"));
+alert(a+b);
+
+console.log(a+b)
