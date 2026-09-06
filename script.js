7f6710a55ccb943a14b50a8a7b46e9eb7bbc0d87
MariamIdrees <thumininu94@gmail.com>
2026-09-06 19:43:49 +0100

    third commit

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // write a prompt that tells a user they are eligible to vote
- let userAge = (prompt("Enter your age"));
- let newVote = userAge >= 18 ? "you can vote" : "you cannoot vote";
+//  let userAge = (prompt("Enter your age"));
+//  let newVote = userAge >= 18 ? "you can vote" : "you cannoot vote";
 
- alert(newVote);
- console.log(newVote)
- console.log (parseInt("userAge"))
-
-
-
-let a = parseInt(prompt("Enter your first number"));
-let b = parseInt(prompt("Enter your second number"));
-alert(a+b);
-
-console.log(a+b)
+//  alert(newVote);
+//  console.log(newVote)
+//  console.log (parseInt("userAge"))
 
 
-let systemName = (prompt("who are you?"))
-if (systemNam === "jennifer" || systemName === "mariam"){
-     alert ("you can enter the STEM lab")}
 
-     else alert ("you do not have access to the STEM lab")
+// let a = parseInt(prompt("Enter your first number"));
+// let b = parseInt(prompt("Enter your second number"));
+// alert(a+b);
+
+// console.log(a+b)
+
+
+// let systemName = (prompt("who are you?"))
+// if (systemNam === "jennifer" || systemName === "mariam"){
+//      alert ("you can enter the STEM lab")}
+
+//      else alert ("you do not have access to the STEM lab")
 
 
 
@@ -33,3 +33,27 @@ if (systemNam === "jennifer" || systemName === "mariam"){
 //   }
 
    
+// let numOne = 34 (prompt ("Enter your first number"))
+// let numTwo = 12 (prompt ("Enter your second number"))
+// let numThree = 48 (prompt ("Enter your third number"))
+
+// if (numOne > numTwo&&numThree ){
+//     alert(`${numOne} is the greatest`)
+// } elseif (numTwo > numOne&&numThree) {
+//     alert (`${numTwo} is the greatest`)
+// } elseif (numThree > numOne&&numTwo ) {
+//     alert (`${numThree} is the greatest`)
+// }
+
+function greeting (name, age) {
+     return (`my name is ${name} and i am ${age} years old  `)
+}
+
+greeting("mariam", 28)
+console.log (greeting)
+
+const newGreeting = (name) => {
+     return ("my name is mariam")
+}
+newGreeting ()
+console.log (newGreeting)
